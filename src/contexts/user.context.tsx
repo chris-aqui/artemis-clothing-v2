613b420b02360c93c91842e3803210d51e80d0c3
Christine <2739361+christine-aqui@users.noreply.github.com>
2023-08-06 15:23:44 -0400

    observer pattern

--- src/contexts/user.context.tsx
+++ src/contexts/user.context.tsx
@@ -5,15 +5,18 @@ import {
 	createUserDocumentFromAuth,
 } from '../utils/firebase/firebase.utils'
 
+// Create a context for the user
 export const UserContext = createContext({
 	setCurrentUser: () => null,
 	currentUser: null,
 });
 
+// Create a provider for the user context
 export const UserProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState(null);
 	const value = { currentUser, setCurrentUser };
 
+	// Listen for changes in the user's authentication state
 	useEffect(() => {
 		const unsubscribe = onAuthStateChangedListener((user) => {
 			if (user) {
@@ -25,5 +28,6 @@ export const UserProvider = ({ children }) => {
 		return unsubscribe;
 	}, []);
 
+	// Provide the user context to the children
 	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
